17f66e6d662d15fc7ae115db6bdd4271b2507a62
Ermag <ermag@student.chalmers.se>
2022-01-26 15:47:23 +0100

    abandoning hw1.1

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as ig
+import numpy.linalg as la
 
 # HW1 Task 1
 def chi(alpha, r):
@@ -18,12 +19,13 @@ def lapPhi(Cs, alphas, r):
         lapPhi += 2 * alpha * (2 * alpha * r**2 - 3) * Cs[i] * chi(alpha,r)
     return lapPhi 
 
-def H(Cs, alphas, rvec):
-   -lapPhi(Cs, alphas, r)/2 - 2/r + ig.quad(np.abs(phi(Cs, alphas, np.abs(rvec)))**2 / np.abs(rvec-Rvec))
-
-
-r = np.linspace(0,1,10)
-print(r)
+def integrand(z2,y2,x2 , r1vec, Cs, alphas):
+    r2vec=np.array([x2,y2,z2])
+    return phi(Cs, alphas, la.norm(r2vec))**2  / la.norm(r1vec-r2vec)
 
 alphas = [0.297104, 1.236745, 5.749982, 38.216677]
 Cs = np.ones(4)
+
+r1vec = 
+
+-lapPhi(Cs, alphas, r1)/2 + ( - 2/r1 + ig.tplquad( integrand(z2,y2,x2,r1vec,Cs,alphas),-100,100) ) * phi(Cs, alphas, r1)
