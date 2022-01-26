3d12d99ec3d67adeb87cf3d01aeb3d0f21260a61
Ermag <ermag@student.chalmers.se>
2022-01-26 18:45:30 +0100

    Task 2 started

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -29,3 +29,47 @@ Cs = np.ones(4)
 r1vec = 
 
 -lapPhi(Cs, alphas, r1)/2 + ( - 2/r1 + ig.tplquad( integrand(z2,y2,x2,r1vec,Cs,alphas),-100,100) ) * phi(Cs, alphas, r1)
+
+
+# HW1 Task 2
+def lap(U,i,h):
+    return (U[i+1]-2*U[i] + U[i-1])/h**2
+
+def realphi(r):
+    return (1/r) - (1 + 1/r) * np.exp(-2*r)
+
+def task2():
+    a=0 
+    b=10 
+    n=1000 
+    h=(b-a)/n
+    rmax=b #idfk
+
+    r = np.linspace(a,b,n)
+
+    U = np.zeros(n)
+    ddU = np.zeros(n)
+    phisquare = np.zeros(n)
+
+    for i in range(n):
+        if i == 0 or n:
+            U[i] = 0
+        U[i]=r[i] * V[i] - r[i]/rmax #detta är sus
+    
+    for i in range(n):
+        if i == 0 or n:
+            U[i] = 0
+        ddU[i] = lap(U,i,h)
+
+    phisquare = - 1/(4*np.pi) * ddU
+
+    plt.plot(r,np.sqrt(phisquare),'r')
+    plt.plot(r,realphi(r),'b')
+    plt.show()
+
+task1()
+task2()
+task3()
+task4()
+task5()
+task6()
