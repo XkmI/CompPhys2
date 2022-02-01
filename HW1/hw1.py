fc5a8c5b656afaabf20b050b48d01dae9affd7b9
Emil Ingelsten <emil.ingelsten@telia.com>
2022-02-01 17:13:53 +0100

    Fixed task 4

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -158,22 +158,22 @@ def task3():
 # HW1 task 4
 def task4():
     rMax = 20
-    nPoints = 2001
+    nPoints = 4001
     nMin2 = nPoints-2
     rs = np.linspace(0,rMax,nPoints)
-    E = np.array([0.0, 1.0])
+    epsList = np.array([0.0, 1.0])
     i = 0
 
     # initial condition
-    u = - 4 * rs[1:-1] * rs[1:-1] * np.exp(-2*rs[1:-1])
+    u = 2 * rs[1:-1] * np.exp(-rs[1:-1]) * np.sqrt(2)
 
-    while np.abs(E[1] - E[0]) > 1e-5:
+    while np.abs(epsList[1] - epsList[0]) > 1e-5:
         i+=1
-        E[0] = E[1]
+        epsList[0] = epsList[1]
         print('------------------')
         print('Iteration ' + str(i))
 
-        f = u**2/rs[1:-1]
+        f = -u**2/rs[1:-1]
         Amat = sp.diags([1, -2, 1], [-1, 0, 1], shape=(nMin2,nMin2)).toarray() / (rs[1] - rs[0])**2
         U0vec = la.solve(Amat,f)
         vsH = U0vec/rs[1:-1] + 1/rMax  #*2?
@@ -181,18 +181,26 @@ def task4():
 
         ei=0
         eps, u = la.eigh(BMat,subset_by_index=[ei,ei])
-        E[1] = eps[0]
-        u = u.flatten()
-        print('Ground state energy for two latest iterations are:')
-        print(E)
- 
+        epsList[1] = eps[0]
+        u = u.flatten()/(np.sqrt(rs[1]-rs[0]))
+
+        #print(np.sum(u**2))
+        print('Eigenvalues for two latest iterations are:')
+        print(epsList)
+    
+    print('------------------')
+    print('Calculated ground state energy:')
+    eG = 2*eps[-1] - np.sum(u**2 * vsH)*(rs[1]-rs[0])
+    print(eG)
+    print('------------------')
+    print('u next to endpoints:')
     print(u[0])
     print(u[-1])
-    plt.plot(rs[1:-1], u/(np.sqrt(rs[1]-rs[0])),linewidth=6, label='Calculated u(r)')
-    plt.plot(rs[1:-1],2*rs[1:-1]*np.exp(-rs[1:-1]),'-.',linewidth=2, label='Theoretical u(r)')
+    plt.plot(rs[1:-1], u, label='Calculated u(r)') # ,linewidth=6
+    #plt.plot(rs[1:-1],2*rs[1:-1]*np.exp(-rs[1:-1]),'-.',linewidth=2, label='Theoretical u(r)')
     plt.xlabel('Radial distance $r$ [$a_0$]')
     plt.ylabel('Radial wave function $u(r)$ [$a_0^{-1/2}$] ')
-    plt.xlim([0,10])
+    plt.xlim([0,8])
     plt.legend()
     plt.show()
     
