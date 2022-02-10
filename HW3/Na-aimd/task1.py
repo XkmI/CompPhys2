970de409cc4ac5ee40a127ca44e51e107f9d898a
Ermag <ermag@student.chalmers.se>
2022-02-11 00:19:08 +0100

    Bugfixing@vera (The most efficient method!)

--- HW3/Na-aimd/task1.py
+++ HW3/Na-aimd/task1.py
@@ -1,16 +1,17 @@
 #Vi behöver nog fler paket
-import ase
-import gpaw
+import ase.io as io
+import ase.md.npt as npt
+from gpaw import GPAW
 from ase.units import fs, kB 
 
-atoms = read('cluster24_9176_Na.xyz')  
+atoms = io.read('cluster24_9176_Na.xyz')  
 
 calc = GPAW(mode = 'lcao', xc = 'PBE', basis = 'dzp', symmetry = {'point_group': False}, charge = 1, txt = 'gpawOutput.gpaw-out')
 
 atoms.set_calculator(calc)
 
-dyn = NPT(atoms, timestep = 0.5*fs, temperature = 350*kB, externalstress = 0, ttime = 20*fs, logfile = 'babys_first_Nacluster24.log')
+dyn = npt.NPT(atoms, timestep = 0.5*fs, temperature = 350*kB, externalstress = 0, ttime = 20*fs, pfactor=None, logfile = 'babys_first_Nacluster24.log')
 
-trajectory = Trajectory('babys_first_Nacluster24.traj', 'w', atoms)
+trajectory = io.Trajectory('babys_first_Nacluster24.traj', 'w', atoms)
 dyn.attach(trajectory.write, interval=1)
 dyn.run(4000)
