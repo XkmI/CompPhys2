0c85d839f56facd175e6ce7a6eaec7c17dab3f25
Ermag <ermag@student.chalmers.se>
2022-02-11 15:03:51 +0100

    task2 progress

--- HW3/Na-aimd/task2.py
+++ HW3/Na-aimd/task2.py
@@ -1,11 +1,17 @@
-import ase as ase
+import ase.io as io
+import numpy as np
 from ase.units import fs, kB 
 
 def partial_rdf(filename):
-    atoms = ase.read(filename)
-    #find the Na atom somehow
-    distances = atoms.get_distances()
-    for atom in atoms:
-        if atom.symbol() == 'H':
-            print('Saltiness detected')
-        if atom.symbol() == 'O':
+    atoms = io.read(filename)
+    n = 100
+    distances = np.zeros(24) #24 oxygen atoms (and water molecules)
+    for i in range(49,72): #Oxygen, this is pretty much the center of each water molecule
+        distances[i-48] = atoms.get_distances(i,48)
+    distances.sort()
+
+    r = np.linspace(0,max(distances),n)  
+
+        
+
+partial_rdf('cluster24_9176_Na.xyz')
