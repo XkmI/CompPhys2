78712eabab881fce32c1196b50f2c7901595ec51
Ermag <ermag@student.chalmers.se>
2022-02-18 19:09:54 +0100

    task 1 done..?

--- HW4/Na-tddft/task1.py
+++ HW4/Na-tddft/task1.py
@@ -1,6 +1,10 @@
+from this import d
+from turtle import width
 from ase import Atoms
 from gpaw import GPAW
 import ase.io as io
+from gpaw.lrtddft import LrTDDFT
+from gpaw.lrtddft import photoabsorption_spectrum
 
 atoms = io.read('Na8.xyz')
 atoms.center(vacuum=8.0) #add 8 ang vacuum
@@ -10,6 +14,16 @@ calc = GPAW(mode='fd', xc='LDA', setups={'Na': '1'}, h=0.3, nbands=0, txt='f.gpa
 atoms.set_calculator(calc)
 atoms.get_potential_energy()
 
-calc.write('babysFirstCalc(task1).gpw', 'all') #saves the calculator, load it by calc=GPAW('babysFirstCalc(task1).gpw')
+calc.write('task1_groundstate.gpw', 'all') #saves the calculator, load it by calc=GPAW('babysFirstCalc(task1).gpw')
 
 calc.set(nbands=110, convergence={'bands': -10}, fixdensity=True)
+
+calc.write('task1_backup.gpw','all')
+
+calc.get_eigenvalues()
+
+dE = 6 # eV
+lr = LrTDDFT(calc, xc='LDA', restrict={'energy range': dE})
+lr.diagonalize()
+photoabsorption_spectrum(lr,'spectrum_w.dat',width=0.06)
+lr.write('LrTDDFTresults.dat') # Save for task 2
