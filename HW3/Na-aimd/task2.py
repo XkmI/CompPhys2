dadd871869b77aaff03afc5d513cb393a8555d9d
Emil Ingelsten <emil.ingelsten@telia.com>
2022-02-22 17:22:33 +0100

    fix bug

--- HW3/Na-aimd/task2.py
+++ HW3/Na-aimd/task2.py
@@ -11,7 +11,7 @@ def partial_rdf(filename=None, atoms=None, dr=None, nBins=None):
     nOx = 24 #24 oxygen atoms (and water molecules)
     distances = np.zeros(nOx) 
     for i in range(nOx): #Finds distances from the sodium atom (#-1 i.e. the last one) to all oxygen atoms (#0-23), since they are more or less at the centre of each water molecule
-        distances[i] = atoms.get_distances(i,-1)
+        distances[i] = atoms.get_distances(i,-1,mic=True)
     distances.sort()
 
     # Set dr and #bins for the ~histogram of oxygen distances
@@ -29,17 +29,17 @@ def partial_rdf(filename=None, atoms=None, dr=None, nBins=None):
         nOxHist[int(bInd)] += 1
 
     prdf = nOxHist * max(distances)**3 / (dr * 3 * nOx * rBins**2)
-    
+    #print(max(distances))
     #plt.plot(rBins,prdf)
     #plt.show()
 
-    return (rBins,prdf)
+    return (rBins,prdf,nOxHist)
 
 # Read in .traj file
 traj = TrajectoryReader("babys_first_NaCluster24.traj") #babys_first_
 # Where to start and how many snapshots
 startInd = 1000
-nSnapshots = 3341
+nSnapshots = 2340
 path = os.getcwd()
 
 dr = 0.1
@@ -47,32 +47,28 @@ nBins = 200
 rLimBins = np.linspace(0,nBins*dr,nBins+1)
 rBins = rLimBins[1:]-0.5*dr
 rdf = np.zeros(nBins)
+nOxHist = np.zeros(nBins)
 
 for i in range(startInd, startInd+nSnapshots):
     atoms = traj[i]
     #print(atoms[0])
-    _,prdf = partial_rdf(atoms=atoms,dr=dr,nBins=nBins)
+    _, prdf, nOxHist_part = partial_rdf(atoms=atoms,dr=dr,nBins=nBins)
     rdf += prdf
+    nOxHist += nOxHist_part
 
 rdf /= nSnapshots
+nOxHist /= nSnapshots
 
-print(dr*sum(rdf[:int(2.5/dr)]))
-print(dr*sum(rdf[:int(4.1/dr)]))
-print(dr*sum(rdf[:int(5.1/dr)]))
-
-print(dr*sum(rdf[:int(3.2/dr)]))
+print(sum(nOxHist[:int(3.2/dr)]))
+print(nOxHist[:int(3.2/dr)])
 
 plt.plot(rBins,rdf,label='Radial distribution function of O around Na')
 
-plt.plot([2.45, 2.45],[-5, 20],'k:', label='Early first minimum ($r = 2.45 a_0$)')
-plt.plot([4.05, 4.05],[-5, 20],'k--', label='Middle first minimum ($r = 4.05 a_0$)')
-plt.plot([5.05, 5.05],[-5, 20],'k-.', label='Late first minimum ($r = 5.05 a_0$)')
-
-#plt.plot([3.15, 3.15],[-5, 20],'k-.', label='First minimum ($r = 3.15 a_0$)')
+plt.plot([3.15, 3.15],[-5, 20],'k-.', label='First minimum ($r = 3.15 a_0$)')
 plt.xlabel('Radial distance $r$ [$a_0$]')
 plt.ylabel('RDF$_\\operatorname{NaO}$ at distance r')
-plt.xlim([-0.1, 10])
-plt.ylim([-0.1, 12.1])
+plt.xlim([-0.1, 8])
+plt.ylim([-0.1, 8])
 plt.legend()
 plt.show()
 plt.savefig('lamao.pdf')
