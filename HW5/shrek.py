a50d9521bc88bbe7f5aab634b732510482994826
Emil Ingelsten <emil.ingelsten@telia.com>
2022-02-25 18:36:39 +0100

    test

--- HW5/shrek.py
+++ HW5/shrek.py
@@ -1,10 +1,10 @@
 import numpy as np
 from ase import Atoms
 from ase.build import bulk
-from ase.io import write
+# from ase.io import write
 # from ase.io.trajectory import PickleTrajectory
 from ase.io.trajectory import TrajectoryWriter
-from ase.optimize.bfgs import BFGS
+# from ase.optimize.bfgs import BFGS
 from gpaw import GPAW
 from gpaw import PW
 # from gpaw import FermiDirac
@@ -29,4 +29,4 @@ for i in range(3):
 
     for x in np.linspace(0.95, 1.05, 7):
         all_that_glitters.set_cell(cell * x, scale_atoms=True)
-        TrajectoryWriter(atomNames[i] + '.traj', mode='a', atoms=all_that_glitters)
+        TrajectoryWriter.write(atomNames[i] + '.traj', mode='a', atoms=all_that_glitters)
