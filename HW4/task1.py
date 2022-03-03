61028449f1e8eba376c4e33ec55f839cd0e60646
Ermag <ermag@student.chalmers.se>
2022-03-03 16:44:28 +0100

    Finally finished the file setup. First bug fixed?

--- HW4/task1.py
+++ HW4/task1.py
@@ -15,19 +15,16 @@ atoms.get_potential_energy()
 
 calc.write('task1_groundstate.gpw', mode='all') #saves the calculator, load it by calc=GPAW('babysFirstCalc(task1).gpw')
 
-atoms, calc2 = restart('task1_groundstate.gpw')
-calc2.set(nbands=110, convergence={'bands': -10}, fixdensity=True)
+#atoms, calc2 = restart('task1_groundstate.gpw')
+#calc2.set(nbands=110, convergence={'bands': -10}, fixdensity=True)
 
-#energy = atoms.get_potential_energy()
-#print(energy)
+#calc2.write('task1_backup.gpw', mode='all')
+#_ , calc3 = restart('task1_backup.gpw')
 
-calc2.write('task1_backup.gpw', mode='all')
-_ , calc3 = restart('task1_backup.gpw')
+#calc3.get_eigenvalues()
 
-calc3.get_eigenvalues()
-
-dE = 6 # eV
-lr = LrTDDFT(calc3, xc='LDA', restrict={'energy range': dE})
-lr.diagonalize()
-photoabsorption_spectrum(lr,'spectrum_w.dat',width=0.06)
-lr.write('LrTDDFTresults.dat') # Save for task 2
+#dE = 6 # eV
+#lr = LrTDDFT(calc3, xc='LDA', restrict={'energy range': dE})
+#lr.diagonalize()
+#photoabsorption_spectrum(lr,'spectrum_w.dat',width=0.06)
+#lr.write('LrTDDFTresults.dat') # Save for task 2
