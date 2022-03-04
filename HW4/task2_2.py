86ea455114c4970e984b06c0b60a32b1935da015
Ermag <ermag@student.chalmers.se>
2022-03-04 17:25:28 +0100

    code works, does it do the right thing though

--- HW4/task2_2.py
+++ HW4/task2_2.py
@@ -1,7 +1,30 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
-from helper import fold
+
+def fold(x_t, x_i, y_i, width):
+    '''
+        Convolutes each peak in the discrete spectrum
+        with a Gaussian of chosen width.
+        
+        inputs:
+        x_t:    vector with energies (i.e. linspace/np.arange)
+        x_i:    stick spectrum energies
+        y_i:    stick spectrum intensities
+        width:  width of the Gaussian
+
+        outputs:
+        y_t:    convoluted spectrum, i.e. intensities
+    '''
+    def Gauss(x0):
+        norm = 1.0 / (width * np.sqrt(2 * np.pi))
+        return norm * np.exp(-0.5 * (x_t - x0)**2 / width**2)
+
+    y_t = np.zeros_like(x_t)
+    for x, y in zip(x_i, y_i):
+        y_t += y * Gauss(x)
+    return y_t
+
 '''
 		Keywords for dumped quantities:
 		'i_p':		Kohn-Sham state that gets excited from
@@ -19,26 +42,29 @@ dump = np.load('babysFirstDump.npz')
 K_pp = dump['K_pp']
 de_p = dump['ediff_p']
 
-mu_p=np.zeros(3)
-mu_p[0] = dump['mux_p']
-mu_p[1] = dump['muy_p']
-mu_p[2] = dump['muz_p']
+#mu_p=np.zeros(3)
+mux_p = dump['mux_p']
+muy_p = dump['muy_p']
+muz_p = dump['muz_p']
 df_p = dump['fdiff_p']
 a_p = dump['a_p']
 i_p = dump['i_p']
 
 D_pp = np.diag(de_p)
-omega_pp = np.block([[np.zeros(D_pp.shape),D_pp],[-D_pp-2*K_pp,np.zeros(D_pp.shape)]]) 
-(X,F) = la.eig(omega_pp) #these are the discrete x vals
+omega_pp = np.sqrt(D_pp)*(D_pp+2*K_pp)*np.sqrt(D_pp)
+(eigs,F) = la.eig(omega_pp) 
+X = np.sqrt(eigs) #these are the discrete x vals
 
 fi=0
+mu = np.array([mux_p, muy_p, muz_p])
+#print(X)
 for a in range(0,2):
     sum = 0
-    for p in len(X):
-        sum += mu_p[a][p] * np.sqrt(df_p[p]*de_p[p]) * F[p]
+    for p in range(0,len(X/2)-1):
+        sum += mu[a][p] * np.sqrt(df_p[p]*de_p[p]) * F[p]
     fai = 2*np.abs(sum)*np.abs(sum)
     fi += fai
-Y = fi #these are the discrete y vals
+Y = fi/3 #these are the discrete y vals
 
 hartreeInEV = 27.211396641308
 x = np.linspace(0,6/hartreeInEV)
@@ -47,3 +73,4 @@ y = fold(x,X,Y,0.06)
 plt.plot(x,y)
 plt.xlabel('Energy [Ha]')
 plt.ylabel('Dipole moment [1/Ha]?')
+plt.show()
