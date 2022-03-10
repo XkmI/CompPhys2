2084a2d5ba226e0f22f823577f0310fbfbd88432
Ermag <ermag@student.chalmers.se>
2022-03-10 20:28:38 +0100

    task4 verad

---
+++ HW5/gas_gas_gas.py
@@ -0,0 +1,18 @@
+from ase import Atoms
+from ase.build import molecule
+from ase.optimize import GPMin, BFGS
+from gpaw import GPAW, PW
+
+o2 = molecule('O2', vacuum=6.0)
+co = molecule('CO', vacuum=6.0)
+
+babys_nth_calc = GPAW(xc ='PBE', mode=PW(450), kpts =(1, 1, 1), spinpol=True, txt='calculation.txt')
+babys_nplus1th_calc = GPAW(xc ='PBE', mode=PW(450), kpts =(1, 1, 1), spinpol=False, txt='calculation.txt')
+
+o2.set_calculator(babys_nth_calc)
+relax = BFGS(o2, trajectory='o2.traj',logfile='o2.log')
+relax.run(fmax=0.02)
+
+co.set_calculator(babys_nplus1th_calc)
+relax = BFGS(co, trajectory='co.traj',logfile='co.log')
+relax.run(fmax=0.02)
